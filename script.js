f56932321acab51966e4b68d27e4773621b6a128
saikireeti06 <143630547+saikireeti06@users.noreply.github.com>
2023-09-08 01:21:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function opentab(tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tab-contents");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tab-links");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active-links");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active-links";
+    console.log(tabcontent);
+}
